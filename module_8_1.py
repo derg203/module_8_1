6ae94eeef49c2ebb37fed741f38cdef0bc6ab8c8
derg203 <ddergacev714@gmail.com>
2025-01-06 20:07:50 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        if isinstance(a, (float, int)) and isinstance(b, str):
+            return a+b
+        elif isinstance(a, str) and isinstance(b, str):
+            return a+b
+        else:
+            raise TypeError
+    except TypeError:
+        return f'{str(a)}{str(b)}'
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
